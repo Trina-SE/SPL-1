58b870f46ee8641c7ba9da40def59bc327dd6ce3
Mosamma Sultana Trina <114928172+Trina-SE@users.noreply.github.com>
2023-05-11 01:37:35 +0600

    Modified

--- Abstract Syntax Tree.cpp
+++ Abstract Syntax Tree.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
-#include "parse.cpp"
+#include "t.cpp"
+#include "p.cpp"
+
 using namespace std;
 
-void delete_E_node(node& n)
+void deleteEnode(node& n)
 {
     for (auto it = n.children.begin(); it != n.children.end();)
     {
-        if (it->label == "E")
+        if (it->label == "E" || it->label == "()" || it->label == "(" || it->label == ")" || it->label == "{" || it->label == "}" || it->label == ";")
         {
             it = n.children.erase(it);
         }
@@ -18,12 +20,119 @@ void delete_E_node(node& n)
     }
 }
 
-void del()
+ofstream output3;
+
+void printAbstractSyntaxTree(node n, int depth)
 {
+    output3 << string(depth, ' ') <<n.label << endl;
 
-    delete_E_node(root);
+    for (node child : n.children)
+    {
+        printAbstractSyntaxTree(child, depth + 4);
 
-    cout << "\nParse Tree After Deletion:\n";
+    }
+}
 
-    printParseTree(root, 0);
+int parseTreeNode=0;
+int abstractSyntaxTreeNode=0;
+
+int main(){
+
+   t();
+
+   parsing();
+
+
+   deleteEnode(root);
+
+   vector<Tree>ast;
+
+     for(int i=0;i<indx;i++){
+        ast.push_back(tree[i]);
+        parseTreeNode++;
+     }
+
+     //"E" node delete
+
+     for(int i=0;i<ast.size();i++){
+        for(int j=0;j<ast[i].child.size();j++){
+            if(ast[i].child[j]=="E"){
+                ast.erase(ast.begin()+i);
+            }
+        }
+     }
+
+     // Brackets and semicolon
+
+     for(int i=0;i<ast.size();i++){
+        for(int j=0;j<ast[i].child.size();j++){
+
+          /*  if(ast[i].child[j]==")" && ast[i].child[j+1]=="{"){
+                ast[i].child.erase(ast[i].child.begin() + j);
+            }*/
+
+            if(ast[i].child[j]==";"){
+                ast[i].child.erase(ast[i].child.begin() + j);
+                //cout<<"*"<<j<<"*";
+                j--;
+            }
+            else if(ast[i].child[j]=="()"){
+                ast[i].child.erase(ast[i].child.begin() + j);
+                //cout<<"*"<<j<<"*";
+                j--;
+            }
+            else if(ast[i].child[j]=="(" || ast[i].child[j]==")"){
+                ast[i].child.erase(ast[i].child.begin() + j);
+                //cout<<"*"<<j<<"*";
+                j--;
+            }
+            else if(ast[i].child[j]=="{" || ast[i].child[j]=="}"){
+                ast[i].child.erase(ast[i].child.begin() + j);
+               // cout<<"*"<<j<<"*";
+               j--;
+            }
+        }
+     }
+
+     for(int i=0;i<ast.size();i++){
+            if(ast[i].child.size()==1){
+                 if(ast[i].child[0]==ast[i].parent){ cout<<"||**";
+                     ast.erase(ast.begin()+i);
+                }
+            }
+        }
+
+     abstractSyntaxTreeNode=ast.size();
+
+   cout << "\nAbstract Syntax Tree:\n";
+   cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+   cout<<"__\"Parent\"__\t\t\t  __\"Child\"__\n";
+   cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+
+    for(int i=0;i<ast.size();i++){
+
+      cout<<ast[i].parent<<"  ------------------------------------------->";
+
+      for(int j=0;j<ast[i].child.size();j++){
+        cout<<ast[i].child[j]<<"  ";
+      }
+      cout<<"\n";
+    //  cout<<"__________________________________________________________\n";
+      //input>>to;
+   }
+
+   output3<<"-----------------------------------------------------------------\n";
+   output3<<"In a Tree Format:\n";
+   output3<<"-----------------------------------------------------------------\n";
+
+    output3.open("AST.txt");
+
+    printAbstractSyntaxTree(root, 0);
+
+    cout<<"\n`````````````````````````````````````````````````\n";
+    cout<<"Calculation:\n";
+    cout<<"`````````````````````````````````````````````````\n";
+
+    cout<<"Total node of Parse Tree: "<<parseTreeNode<<"\n";
+    cout<<"Total node of Abstract Syntax Tree: "<<abstractSyntaxTreeNode<<"\n";
 }
